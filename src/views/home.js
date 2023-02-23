28d9af40a4bff7ba066e54d5f244d86626d89064
jnealon19 <99342720+jnealon19@users.noreply.github.com>
2023-02-23 16:19:34 -0500

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,276 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import NavigationLinks from '../components/navigation-links'
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Portfolio Page</title>
+        <meta property="og:title" content="Portfolio Page" />
+      </Helmet>
+      <header data-role="Header" className="home-header">
+        <img
+          alt="logo"
+          src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+          className="home-image"
+        />
+        <div className="home-nav">
+          <NavigationLinks rootClassName="rootClassName17"></NavigationLinks>
+        </div>
+        <div data-role="BurgerMenu" className="home-burger-menu">
+          <svg viewBox="0 0 1024 1024" className="home-icon">
+            <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+          </svg>
+        </div>
+        <div data-role="MobileMenu" className="home-mobile-menu">
+          <div className="home-nav1">
+            <div className="home-container01">
+              <img
+                alt="image"
+                src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+                className="home-image1"
+              />
+              <div
+                data-role="CloseMobileMenu"
+                className="home-close-mobile-menu"
+              >
+                <svg viewBox="0 0 1024 1024" className="home-icon02">
+                  <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                </svg>
+              </div>
+            </div>
+            <NavigationLinks rootClassName="rootClassName18"></NavigationLinks>
+          </div>
+          <div>
+            <svg viewBox="0 0 950.8571428571428 1024" className="home-icon04">
+              <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+            </svg>
+            <svg viewBox="0 0 877.7142857142857 1024" className="home-icon06">
+              <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+            </svg>
+            <svg viewBox="0 0 602.2582857142856 1024" className="home-icon08">
+              <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+            </svg>
+          </div>
+        </div>
+      </header>
+      <div className="home-container02">
+        <div className="home-hero">
+          <div className="home-hero-text-container">
+            <h1 className="home-heading Section-Heading">
+              I help startups deliver the right message to their customers
+            </h1>
+            <span className="home-text Section-Text">
+              <span>
+                c. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed
+                augue semper porta. Mauris massa. Vestibulum lacinia arcu eget
+                nulla. Class aptent taciti sociosqu ad litora torquent per
+                conubia nostra, per inceptos himenaeos.
+              </span>
+            </span>
+            <div className="home-cta-btn-container">
+              <button className="home-cta-btn Anchor button">
+                <span className="home-text02">MY WORK</span>
+              </button>
+              <button className="home-cta-btn1 button Anchor">ABOUT ME</button>
+            </div>
+          </div>
+          <img
+            alt="image"
+            src="https://play.teleporthq.io/static/svg/default-img.svg"
+            className="home-image2"
+          />
+        </div>
+      </div>
+      <div id="features" className="home-features">
+        <div className="home-heading-container">
+          <h2 className="home-text03 Section-Heading">Features</h2>
+          <span className="home-text04 Section-Text">
+            Check out all the cool things you can do with this PlowPal website.
+          </span>
+        </div>
+        <div className="home-cards-container">
+          <div className="home-card">
+            <div className="home-icon-container">
+              <svg
+                viewBox="0 0 1170.2857142857142 1024"
+                className="home-icon10"
+              >
+                <path d="M1170.286 877.714v73.143h-1170.286v-877.714h73.143v804.571h1097.143zM1097.143 164.571v248.571c0 16-19.429 24.571-31.429 12.571l-69.143-69.143-361.714 361.714c-7.429 7.429-18.857 7.429-26.286 0l-133.143-133.143-237.714 237.714-109.714-109.714 334.286-334.286c7.429-7.429 18.857-7.429 26.286 0l133.143 133.143 265.143-265.143-69.143-69.143c-12-12-3.429-31.429 12.571-31.429h248.571c10.286 0 18.286 8 18.286 18.286z"></path>
+              </svg>
+            </div>
+            <div className="home-content-container">
+              <span className="home-heading1 Card-Heading">
+                Manually Deploy
+              </span>
+              <span className="home-text05 Card-Text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero. Sed cursus ante dapibus diam.c Sed
+                nisi. Nulla quis sem at nibh elementum imperdiet.
+              </span>
+            </div>
+          </div>
+          <div className="home-card1">
+            <div className="home-icon-container1">
+              <svg viewBox="0 0 1024 1024" className="home-icon12">
+                <path d="M810 128q34 0 60 26t26 60v256h-214l44-172-172 44v-214h256zM726 726l-44-172h214v256q0 34-26 60t-60 26h-256v-214zM342 554l-44 172 172-44v214h-256q-34 0-60-26t-26-60v-256h214zM128 214q0-34 26-60t60-26h256v214l-172-44 44 172h-214v-256z"></path>
+              </svg>
+            </div>
+            <div className="home-content-container1">
+              <span className="home-heading2 Card-Heading">Create a Route</span>
+              <span className="home-text06 Card-Text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero. Sed cursus ante dapibus diam.c Sed
+                nisi. Nulla quis sem at nibh elementum imperdiet.
+              </span>
+            </div>
+          </div>
+          <div className="home-card2">
+            <div className="home-icon-container2">
+              <svg viewBox="0 0 865.7188571428571 1024" className="home-icon14">
+                <path d="M207.429 877.714l52-52-134.286-134.286-52 52v61.143h73.143v73.143h61.143zM506.286 347.429c0-7.429-5.143-12.571-12.571-12.571-3.429 0-6.857 1.143-9.714 4l-309.714 309.714c-2.857 2.857-4 6.286-4 9.714 0 7.429 5.143 12.571 12.571 12.571 3.429 0 6.857-1.143 9.714-4l309.714-309.714c2.857-2.857 4-6.286 4-9.714zM475.429 237.714l237.714 237.714-475.429 475.429h-237.714v-237.714zM865.714 292.571c0 19.429-8 38.286-21.143 51.429l-94.857 94.857-237.714-237.714 94.857-94.286c13.143-13.714 32-21.714 51.429-21.714s38.286 8 52 21.714l134.286 133.714c13.143 13.714 21.143 32.571 21.143 52z"></path>
+              </svg>
+            </div>
+            <div className="home-content-container2">
+              <span className="home-heading3 Card-Heading">
+                View Deployment Schedule
+              </span>
+              <span className="home-text07 Card-Text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero. Sed cursus ante dapibus diam.c Sed
+                nisi. Nulla quis sem at nibh elementum imperdiet.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home-steps">
+        <h1 className="home-text08">Discover Our Process</h1>
+        <span className="home-text09">
+          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+          <br></br>
+          <span></span>
+        </span>
+        <div className="home-container03">
+          <div className="home-step">
+            <div className="home-container04">
+              <div className="home-line"></div>
+              <div className="home-container05">
+                <svg viewBox="0 0 1024 1024" className="home-icon16">
+                  <path d="M634 558q92-64 92-174 0-88-63-151t-151-63-151 63-63 151q0 46 27 96t65 78l36 26v98h172v-98zM512 86q124 0 211 87t87 211q0 156-128 244v98q0 18-12 30t-30 12h-256q-18 0-30-12t-12-30v-98q-128-88-128-244 0-124 87-211t211-87zM384 896v-42h256v42q0 18-12 30t-30 12h-172q-18 0-30-12t-12-30z"></path>
+                </svg>
+              </div>
+              <div className="home-line1"></div>
+            </div>
+            <div className="home-container06">
+              <h1 className="home-text12">
+                <span>Ideate</span>
+              </h1>
+              <span className="home-text14">
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing.</span>
+                <br></br>
+                <span></span>
+              </span>
+            </div>
+          </div>
+          <div className="home-step1">
+            <div className="home-container07">
+              <div className="home-line2"></div>
+              <div className="home-container08">
+                <svg viewBox="0 0 1024 1024" className="home-icon18">
+                  <path d="M746 512q26 0 45-18t19-46-19-46-45-18-45 18-19 46 19 46 45 18zM618 342q26 0 45-19t19-45-19-45-45-19-45 19-19 45 19 45 45 19zM406 342q26 0 45-19t19-45-19-45-45-19-45 19-19 45 19 45 45 19zM278 512q26 0 45-18t19-46-19-46-45-18-45 18-19 46 19 46 45 18zM512 128q158 0 271 100t113 242q0 88-63 150t-151 62h-74q-28 0-46 19t-18 45q0 22 16 42t16 44q0 28-18 46t-46 18q-160 0-272-112t-112-272 112-272 272-112z"></path>
+                </svg>
+              </div>
+              <div className="home-line3"></div>
+            </div>
+            <div className="home-container09">
+              <h1 className="home-text17">
+                <span>Design</span>
+              </h1>
+              <span className="home-text19">
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing.</span>
+                <br></br>
+                <span></span>
+              </span>
+            </div>
+          </div>
+          <div className="home-step2">
+            <div className="home-container10">
+              <div className="home-line4"></div>
+              <div className="home-container11">
+                <svg viewBox="0 0 1024 1024" className="home-icon20">
+                  <path d="M576 736l96 96 320-320-320-320-96 96 224 224z"></path>
+                  <path d="M448 288l-96-96-320 320 320 320 96-96-224-224z"></path>
+                </svg>
+              </div>
+              <div className="home-line5"></div>
+            </div>
+            <div className="home-container12">
+              <h1 className="home-text22">
+                <span>Develop</span>
+              </h1>
+              <span className="home-text24">
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing.</span>
+                <br></br>
+                <span></span>
+              </span>
+            </div>
+          </div>
+          <div className="home-step3">
+            <div className="home-container13">
+              <div className="home-line6"></div>
+              <div className="home-container14">
+                <svg viewBox="0 0 1024 1024" className="home-icon23">
+                  <path d="M480 64c-265.096 0-480 214.904-480 480 0 265.098 214.904 480 480 480 265.098 0 480-214.902 480-480 0-265.096-214.902-480-480-480zM751.59 704c8.58-40.454 13.996-83.392 15.758-128h127.446c-3.336 44.196-13.624 87.114-30.68 128h-112.524zM208.41 384c-8.58 40.454-13.996 83.392-15.758 128h-127.444c3.336-44.194 13.622-87.114 30.678-128h112.524zM686.036 384c9.614 40.962 15.398 83.854 17.28 128h-191.316v-128h174.036zM512 320v-187.338c14.59 4.246 29.044 11.37 43.228 21.37 26.582 18.74 52.012 47.608 73.54 83.486 14.882 24.802 27.752 52.416 38.496 82.484h-155.264zM331.232 237.516c21.528-35.878 46.956-64.748 73.54-83.486 14.182-10 28.638-17.124 43.228-21.37v187.34h-155.264c10.746-30.066 23.616-57.68 38.496-82.484zM448 384v128h-191.314c1.88-44.146 7.666-87.038 17.278-128h174.036zM95.888 704c-17.056-40.886-27.342-83.804-30.678-128h127.444c1.762 44.608 7.178 87.546 15.758 128h-112.524zM256.686 576h191.314v128h-174.036c-9.612-40.96-15.398-83.854-17.278-128zM448 768v187.34c-14.588-4.246-29.044-11.372-43.228-21.37-26.584-18.74-52.014-47.61-73.54-83.486-14.882-24.804-27.75-52.418-38.498-82.484h155.266zM628.768 850.484c-21.528 35.876-46.958 64.746-73.54 83.486-14.184 9.998-28.638 17.124-43.228 21.37v-187.34h155.266c-10.746 30.066-23.616 57.68-38.498 82.484zM512 704v-128h191.314c-1.88 44.146-7.666 87.040-17.28 128h-174.034zM767.348 512c-1.762-44.608-7.178-87.546-15.758-128h112.524c17.056 40.886 27.344 83.806 30.68 128h-127.446zM830.658 320h-95.9c-18.638-58.762-44.376-110.294-75.316-151.428 42.536 20.34 81.058 47.616 114.714 81.272 21.48 21.478 40.362 44.938 56.502 70.156zM185.844 249.844c33.658-33.658 72.18-60.932 114.714-81.272-30.942 41.134-56.676 92.666-75.316 151.428h-95.898c16.138-25.218 35.022-48.678 56.5-70.156zM129.344 768h95.898c18.64 58.762 44.376 110.294 75.318 151.43-42.536-20.34-81.058-47.616-114.714-81.274-21.48-21.478-40.364-44.938-56.502-70.156zM774.156 838.156c-33.656 33.658-72.18 60.934-114.714 81.274 30.942-41.134 56.678-92.668 75.316-151.43h95.9c-16.14 25.218-35.022 48.678-56.502 70.156z"></path>
+                </svg>
+              </div>
+              <div className="home-line7"></div>
+            </div>
+            <div className="home-container15">
+              <h1 className="home-text27">
+                <span>Deploy</span>
+              </h1>
+              <span className="home-text29">
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing.</span>
+                <br></br>
+                <span></span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="home-container16">
+          <div className="home-container17">
+            <div className="home-container18">
+              <h1 className="home-text32">Select Route:</h1>
+            </div>
+            <select id="selectRouteDD" required className="home-select">
+              <option value="Option 1">Option 1</option>
+              <option value="Option 2">Option 2</option>
+              <option value="Option 3">Option 3</option>
+            </select>
+          </div>
+          <div className="home-container19">
+            <div className="home-container20">
+              <h1 className="home-text33">Deploy Robot</h1>
+            </div>
+            <button
+              id="deployButton"
+              name="Deploy"
+              type="submit"
+              className="home-button button"
+            >
+              Deploy
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="home-section-separator"></div>
+    </div>
+  )
+}
+
+export default Home
